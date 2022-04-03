c76e3b9af239c4856b6d5dcfc10a874aa5e50344
Jinneon <Jinneon@users.noreply.github.com>
2022-04-03 11:42:23 +0300

    UI FIX

---
+++ Gamejam2022-main/Gamejam2022-main/Gamejam2022-main/Gamejam2022-main/Jam2022/Assets/Scripts/CloudHeal.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloudHeal : Character
+{
+    public SpriteRenderer spriterenderer;
+    public float playerHealth;
+    public float howManyHearts;
+    public Image[] heartImages;
+    public Sprite heart, noHeart, halfHeart;
+    public List<int> uiCount;
+
+    private bool invincibilityTime = false;
+    private UnityEngine.Object flashParticle;
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = CharacterHp = 3f;
+        flashParticle = Resources.Load("FlashParticle");
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("--");
+            CharacterHp -= 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            CharacterHp += 0.5f;
+        }
+        playerHealth = CharacterHp;
+        //   Debug.Log("Player HP:" + playerHealth);
+
+        if (playerHealth > howManyHearts)
+        {
+            playerHealth = howManyHearts;
+        }
+        if (playerHealth <= 0)
+        {
+            Die();
+            playerHealth = 0;
+            Debug.Log("Game over");
+        }
+
+        // Debug.Log(i + " playerheart is " + playerHealth);
+        if (playerHealth == 3)
+        {
+            heartImages[0].sprite = heart;
+            heartImages[1].sprite = heart;
+            heartImages[2].sprite = heart;
+
+        }
+        else if (playerHealth == 2.5)
+        {
+            heartImages[0].sprite = heart;
+            heartImages[1].sprite = heart;
+            heartImages[2].sprite = halfHeart;
+        }
+        else if (playerHealth == 2)
+        {
+            heartImages[0].sprite = heart;
+            heartImages[1].sprite = heart;
+            heartImages[2].sprite = noHeart;
+        }
+        else if (playerHealth == 1.5)
+        {
+            heartImages[0].sprite = heart;
+            heartImages[1].sprite = halfHeart;
+            heartImages[2].sprite = noHeart;
+        }
+        else if (playerHealth == 1)
+        {
+            heartImages[0].sprite = heart;
+            heartImages[1].sprite = noHeart;
+            heartImages[2].sprite = noHeart;
+        }
+        else if (playerHealth == 0.5)
+        {
+            heartImages[0].sprite = halfHeart;
+            heartImages[1].sprite = noHeart;
+            heartImages[2].sprite = noHeart;
+        }
+        else if (playerHealth == 0)
+        {
+            heartImages[0].sprite = noHeart;
+        }
+    }
+
+    public override void OnDamage(float damage)
+    {
+        anim.SetTrigger("Cat_Hit");
+        if (!invincibilityTime)
+        {
+            CharacterHp -= damage;
+
+            invincibilityTime = true;
+            StartCoroutine(iTime());
+        }
+    }
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.2f);
+        //   anim.SetBool("Cat_Hit", false);
+    }
+
+    public override void Die()
+    {
+        isCharacterDead = true;
+        GameManager.GetInstance().even?.Invoke();
+        anim.SetBool("IsDead", true);
+        /*  GameObject flash = (GameObject)Instantiate(flashParticle);
+          flash.transform.position = new Vector3(transform.position.x, transform.position.y + .3F, transform.position.z);
+          Destroy(gameObject);*/
+    }
+
+    IEnumerator iTime()
+    {
+
+
+        int countTime = 0;
+
+        while (countTime < 10)
+        {
+            if (countTime % 2 == 0)
+                spriterenderer.color = new Color32(255, 255, 255, 90);
+            else
+                spriterenderer.color = new Color32(255, 255, 255, 180);
+
+            countTime++;
+            yield return new WaitForSeconds(0.2f);
+        }
+        spriterenderer.color = new Color32(255, 255, 255, 255);
+        invincibilityTime = false;
+
+        yield return null;
+    }
+}
